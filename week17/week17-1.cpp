b26cad2c2b4efacdbd7a3f9c46610ab96e963514
Yuuta Huang <91250050+yuutahuang@users.noreply.github.com>
2022-01-06 16:36:16 +0800

    Add files via upload

---
+++ week17/week17-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n=1234567892;///拜计琌ぶ
+    /*
+    n%10 緇计碞琌计
+    n/10 埃10玡计
+    */
+    while(n>0){
+        printf("%d => %d %d \n",n,n/10,n%10);
+        n = n/10;
+    }
+}
---
+++ week17/week17-3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n=1234567892;///拜计琌ぶ
+    /*
+    n%10 緇计碞琌计
+    n/10 埃10玡计
+    */
+    int sum = 0;
+    while(n>0){
+        printf("%d => %d %d \n",n,n/10,n%10);
+        sum += n%10;
+        n = n/10;
+    }
+    printf("%d",sum);
+}
+
+
